8fedae674f8e7e8a47eb20b639c0ebc42bc96c6e
Christopher Hunter <chuntr@users.noreply.github.com>
2023-03-20 23:17:39 -0700

    Adding new files

---
+++ app/src/main/java/com/example/distrubutingdata/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.distrubutingdata;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private EditText mMessageEditText;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
+
+    public static final String EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // listener for the send button
+        findViewById(R.id.button_search).setOnClickListener(v -> {
+            launchSecondActivity();
+        });
+
+    }
+
+    public void launchSecondActivity() {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+}
---
+++ app/src/main/java/com/example/distrubutingdata/SecondActivity.java
@@ -0,0 +1,4 @@
+package com.example.distrubutingdata;
+
+public class SecondActivity {
+}
