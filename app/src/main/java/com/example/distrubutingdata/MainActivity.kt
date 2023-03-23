3f942cb9e0537c034bac38c013e5ef5e19d8e8ce
Christopher Hunter <chuntr@users.noreply.github.com>
2023-03-22 19:23:45 -0700

    added bindPreview(), launchMainActiivty(), launchResultsActivity(), dispatchTakePitctureIntent(), takePhoto(), and generally migrated stuff from MainActivity.java that was missing

--- app/src/main/java/com/example/distrubutingdata/MainActivity.kt
+++ app/src/main/java/com/example/distrubutingdata/MainActivity.kt
@@ -1,27 +1,30 @@
 package com.android.example.cameraxapp
 
+import android.content.ActivityNotFoundException
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import android.widget.EditText
+import android.widget.TextView
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.CameraX
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.video.Recorder
 import androidx.camera.video.Recording
 import androidx.camera.video.VideoCapture
-import com.example.distrubutingdata.MainActivity
-import com.example.distrubutingdata.R
-import com.example.distrubutingdata.SecondActivity
-import com.example.distrubutingdata.databinding.ActivityMainBinding
+import com.example.distrubutingdata.ResultsActivity
+import com.google.common.util.concurrent.ListenableFuture
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -29,53 +32,94 @@ typealias LumaListener = (luma: Double) -> Unit
 
 
 class MainActivity : AppCompatActivity() {
-   //private lateinit var viewBinding: ActivityMainBinding
-    private lateint var viewBinding: ResultProfileBinding
+    //private lateint var binding: ActivityMainBinding
+    private lateint var binding: ActivityMainBinding?
+    //private lateint var binding: MainActivityBinding?
+    private val resultsView: TextView? = null
+    private var imageCapture: ImageCapture? = null
+    private var videoCapture: VideoCapture<Recorder>? = null
+    private var recording: Recording? = null
+    private var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>? = null
+    private lateinit var cameraExecutor: ExecutorService
 
-   private var imageCapture: ImageCapture? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_main)        // old method
 
-   private var videoCapture: VideoCapture<Recorder>? = null
-   private var recording: Recording? = null
+        // for camera
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture!!.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(this))
 
-   private lateinit var cameraExecutor: ExecutorService
-
-   override fun onCreate(savedInstanceState: Bundle?) {
-       super.onCreate(savedInstanceState)
-       //val viewBinding: MyLayoutBinding = MyLayoutBinding.inflate(layoutInflater)
-       viewBinding = ResultProfileBinding.inflate(layoutInflater)
-       val view = viewBinding.root
-       setContentView(viewBinding.root)
-
-       // Request camera permissions
-       if (allPermissionsGranted()) {
+        // Request camera permissions
+        if (allPermissionsGranted()) {
            startCamera()
-       } else {
+        } else {
            ActivityCompat.requestPermissions(
                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
-       }
+        }
 
-       // Set up the listeners for take photo and video capture buttons
-       viewBinding.imageCaptureButton take_photo_view = findViewById<View>(R.id.button_take_photo);
-       viewBinding.imageCaptureButton.setOnClickListener { takePhoto() }
-       viewBinding.videoCaptureButton.setOnClickListener { captureVideo() }
+        // Set up the listeners for take photo and video capture buttons
+        //binding.imageCaptureButton take_photo_view = findViewById<View>(R.id.button_take_photo);
+        binding.imageCaptureButton.setOnClickListener { takePhoto() }
+        //binding.videoCaptureButton.setOnClickListener { captureVideo() }  // we don't have a video capture button
 
-       cameraExecutor = Executors.newSingleThreadExecutor()
-       // listener for the send button
+        cameraExecutor = Executors.newSingleThreadExecutor()
 
+        // listener for the submit button
+        //findViewById<View>(R.id.button_submit).setOnClickListener { v: View? -> launchResultsActivity() }
+        binding.imageSubmitButton.setOnClickListener { takePhoto() }
+        binding.name.text = viewModel.name
+        binding.button
 
-       // listener for the send button
-       findViewById<View>(R.id.button_submit).setOnClickListener { v: View? -> launchSecondActivity() }
+    }
 
-       // setup button listeners
-   }
+    fun bindPreview(cameraProvider : ProcessCameraProvider) {
+        var preview : Preview = Preview.Builder()
+                .build()
 
-    fun launchSecondActivity() {
-        //Log.d(MainActivity.LOG_TAG, "Button clicked!")
-        val intent = Intent(this, SecondActivity::class.java)
+        var cameraSelector : CameraSelector = CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
+
+        preview.setSurfaceProvider(previewView.getSurfaceProvider())
+
+        var camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+    }
+    fun launchMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivityForMain(intent, MainActivity.TEXT_REQUEST)
+    }
+    
+    private fun startActivityForMain(this: Intent, textRequest: Int) {
+
+    }
+
+    fun launchResultsActivity() {
+        Log.d(MainActivity.LOG_TAG, "photo submitted!")
+        val intent = Intent(this, ResultsActivity::class.java)
         startActivityForResult(intent, MainActivity.TEXT_REQUEST)
     }
 
-    private fun takePhoto() {}
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, MainActivity.REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+        }
+    }
+
+    private fun takePhoto() {
+        requireActivity().run{
+            startActivity(Intent(this, CameraX))
+        }
+    }
 
     private fun captureVideo() {}
 
@@ -90,7 +134,7 @@ class MainActivity : AppCompatActivity() {
             val preview = Preview.Builder()
                     .build()
                     .also {
-                        it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
+                        it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
                     }
 
             // Select back camera as a default
@@ -116,26 +160,10 @@ class MainActivity : AppCompatActivity() {
            baseContext, it) == PackageManager.PERMISSION_GRANTED
    }
 
-
     override fun onDestroy() {
        super.onDestroy()
        cameraExecutor.shutdown()
    }
-
-    companion object {
-        private const val TAG = "CameraXApp"
-        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
-        private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS =
-            mutableListOf (
-                Manifest.permission.CAMERA,
-                Manifest.permission.RECORD_AUDIO
-            ).apply {
-                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
-                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                }
-            }.toTypedArray()
-    }
 
     override fun onRequestPermissionsResult(
             requestCode: Int, permissions: Array<String>, grantResults:
@@ -152,35 +180,42 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun startMyCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+    companion object {
+        private val LOG_TAG = MainActivity::class.java.simpleName
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
 
-        cameraProviderFuture.addListener({
-            // Used to bind the lifecycle of cameras to the lifecycle owner
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-
-            // Preview
-            val preview = Preview.Builder()
-                    .build()
-                    .also {
-                        it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
+        const val EXTRA_MESSAGE = "com.example.distributingdata.extra.MESSAGE"
+        const val TEXT_REQUEST = 1
+        const val REQUEST_IMAGE_CAPTURE = 1
+        private val REQUIRED_PERMISSIONS =
+                mutableListOf (
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.RECORD_AUDIO
+                ).apply {
+                    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                        add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
                     }
-
-            // Select back camera as a default
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            try {
-                // Unbind use cases before rebinding
-                cameraProvider.unbindAll()
-
-                // Bind use cases to camera
-                cameraProvider.bindToLifecycle(
-                        this, cameraSelector, preview)
-
-            } catch(exc: Exception) {
-                Log.e(TAG, "Use case binding failed", exc)
-            }
-
-        }, ContextCompat.getMainExecutor(this))
+                }.toTypedArray()
     }
+
+/*
+// TextureView is required for the camera to show in the assigned pane
+private val surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+override fun onSurfaceTextureAvailable(texture: SurfaceTexture, width: Int, height: Int) {
+
+}
+override fun onSurfaceTextureSizeChanged(texture: SurfaceTexture, width: Int, height: Int) {
+
+}
+
+override fun onSurfaceTextureDestroyed(texture: SurfaceTexture) {
+
+}
+override fun onSurfaceTextureUpdated(texture: SurfaceTexture) {
+
+}
+}*/
+
 }
